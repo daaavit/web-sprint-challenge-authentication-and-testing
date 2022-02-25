a18fafe7d455e1e16a3c6ef11dd58aa0b0736799
jabaracconich <87547340+jabaracconich@users.noreply.github.com>
2022-02-25 14:40:47 -0500

    restricted

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,25 @@
+const { JWT_SECRET } = require('../secrets')
+const jwt = require('jsonwebtoken')
+
 module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if(!token) 
+  return next({
+    stastus: 401, 
+    message: 'token required'
+  })
+  
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if(err) {
+      next({
+        status: 401, 
+        message: 'Not valid TOKENS' })
+    }else {
+      req.decodedToken = decodedToken;
+      next()
+    }
+  })
   next();
-  /*
-    IMPLEMENT
+ 
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
 };
--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,13 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+const bcrypt = require('bcryptjs')
+const jwtDecode =require('jwt-decode')
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+//need write tests!!!
